20d545d945707527c909ea18f751da5f8f5ef4ed
Ed <edward.blewitt2@gmail.com>
2023-10-09 13:58:55 +0100

    Updated LinkedIn URL

--- index.php
+++ index.php
@@ -84,14 +84,14 @@
                 </li> -->
             </ul>
 
-            <a target="_blank" href="https://www.linkedin.com/in/edward-blewitt-a1b257224/" class="button mobile-only flex align-center gap-s">
+            <a target="_blank" href="https://www.linkedin.com/in/edward-blewitt/" class="button mobile-only flex align-center gap-s">
                 <span>View My LinkedIn</span>
                 <img style="width: 1.2em; height: 1.2em; object-fit: contain;" src="/resources/images/icons/linkedin-accent.svg">
             </a>
 
         </nav>
 
-        <a target="_blank" href="https://www.linkedin.com/in/edward-blewitt-a1b257224/" class="button flex align-center gap-s">
+        <a target="_blank" href="https://www.linkedin.com/in/edward-blewitt/" class="button flex align-center gap-s">
             <span>View My LinkedIn</span>
             <img style="width: 1.2em; height: 1.2em; object-fit: contain;" src="/resources/images/icons/linkedin-accent.svg">
         </a>
