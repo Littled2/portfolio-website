e91a53b1354841233a5aa4c549439447a2228814
Ed <edward.blewitt2@gmail.com>
2023-10-23 16:19:43 +0100

    Added theme colour for mobile devices

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#131313">
 
     <!-- Import the Client Side Router -->
     <!-- Remove this if you don't wish to use the client-side routing function of the Headless CMS -->
